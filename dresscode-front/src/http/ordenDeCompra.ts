47132fed484efc0e11ebd3346ea6692a173f7be9
Lauti <lautaro3314@hotmail.com>
2025-06-06 18:13:33 -0300

    hhtp terminados, revisar imagenes de producto

--- dresscode-front/src/http/ordenDeCompra.ts
+++ dresscode-front/src/http/ordenDeCompra.ts
@@ -1,16 +1,78 @@
-//Revisar si el archivo es correcto
+import type { EstadoOrden } from "../types/enums/EstadoOrden";
+import type { OrdenDeCompra } from "../types/OrdenDeCompra";
+
 
 const baseUrl = import.meta.env.VITE_API_URL;
 
 export const getOrdenDeCompra = async (id: number) => {
   try {
-    const response = await fetch(`${baseUrl}/ordenDeCompra/${id}`);
+    const response = await fetch(`${baseUrl}/ordenes-de-compra/${id}`);
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
     return await response.json();
   } catch (error) {
     console.error("Error fetching orden de compra:", error);
+    throw error;
+  }
+};
+
+export const getOrdenesPorUsuario = async (usuarioId: number) => {
+  try {
+    const response = await fetch(
+      `${baseUrl}/ordenes-de-compra/usuario/${usuarioId}`
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching orden de compra por usuario:", error);
+    throw error;
+  }
+};
+
+
+export const actualizarDetalleOrdenDeCompra = async (
+  detalleId: number,
+  ordenDeCompra: EstadoOrden
+) => {
+  try {
+    const response = await fetch(
+      `${baseUrl}/ordenes-de-compra/detalle/${detalleId}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ estado: ordenDeCompra }),
+      }
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Error updating detalle orden:", error);
+    throw error;
+  }
+};
+
+export const crearOrdenDeCompra = async (ordenDeCompra: OrdenDeCompra) => {
+  try {
+    const response = await fetch(`${baseUrl}/ordenes-de-compra`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(ordenDeCompra),
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Error creating orden de compra:", error);
     throw error;
   }
 }
--- dresscode-front/src/http/talle.ts
+++ dresscode-front/src/http/talle.ts
@@ -1,3 +1,4 @@
+import type { Producto } from "../types/Producto";
 import type { Talle } from "../types/Talle";
 
 const baseUrl = import.meta.env.VITE_API_URL;
@@ -50,6 +51,66 @@ export const actualizarTalle = async (id: number, talle: Talle) => {
     return await response.json();
   } catch (error) {
     console.error('Error updating talle:', error);
+    throw error;
+  }
+}
+
+export const activarTalle = async (id: number) => {
+  try {
+    const response = await fetch(`${baseUrl}/talles/${id}/activate`, {
+      method: 'PUT',
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error('Error activating talle:', error);
+    throw error;
+  }
+}
+
+export const desactivarTalle = async (id: number) => {
+  try {
+    const response = await fetch(`${baseUrl}/talles/${id}/desactivate`, {
+      method: 'PUT',
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error('Error deactivating talle:', error);
+    throw error;
+  }
+}
+
+export const asignarTalleAProducto = async (productoId: Producto, talleId: Talle) => {
+  try {
+    const response = await fetch(`${baseUrl}/${talleId.id}/productos/${productoId}`, {
+      method: 'POST',
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error('Error assigning talle to producto:', error);
+    throw error;
+  }
+}
+
+export const eliminarTalleDeProducto = async (productoId: Producto, talleId: Talle) => {
+  try {
+    const response = await fetch(`${baseUrl}/${talleId.id}/productos/${productoId.id}`, {
+      method: 'DELETE',
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error('Error removing talle from producto:', error);
     throw error;
   }
 }
