73e85edbd36b2e2b361b9e906c11b909fae6f831
Marcos <marcosdimecom@gmail.com>
2025-06-09 20:54:14 -0300

    Detalles productos arreglados

--- dresscode-front/src/types/ProductoTalle.ts
+++ dresscode-front/src/types/ProductoTalle.ts
@@ -1,7 +1,9 @@
+import type { Talle } from "./Talle";
 
 export interface ProductoTalle {
   id?: number; 
+  activo : boolean;
   productoId: number; 
-  talleId: number; 
+  talle: Talle; 
   cantidad: number;
 } 
--- dresscode-front/src/types/Talle.ts
+++ dresscode-front/src/types/Talle.ts
@@ -1,9 +1,7 @@
-import type { ProductoTalle } from "./ProductoTalle";
 
 export interface Talle {
     id?: number;
     activo: boolean;
     tipoTalle: string;
-    productos?: ProductoTalle[];
 }
 
