db6e9f30c06a93ca7e5f7607577aa75ec3411916
Lauti <lautaro3314@hotmail.com>
2025-06-10 19:20:37 -0300

    productos de la api añadidos al home

--- dresscode-front/src/components/ui/OffersSection/OffersSection.tsx
+++ dresscode-front/src/components/ui/OffersSection/OffersSection.tsx
@@ -3,51 +3,22 @@ import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
 import ProductCard from "../ProductCard/ProductCard"; // Ajusta la ruta si es necesario
-
-const offers = [
-  {
-    id: 1,
-    name: "NIKE DUNK LOW",
-    image: "/public/assets/ImagesProducts/image 8.png",
-    discount: 50,
-    price: 130000,
-    oldPrice: 260000,
-  },
-  {
-    id: 2,
-    name: "ADIDAS CAMPOS 90'",
-    image: "/public/assets/ImagesProducts/image 9.png",
-    discount: 30,
-    price: 113000,
-    oldPrice: 190000,
-  },
-  {
-    id: 3,
-    name: "VV",
-    image: "/public/assets/ImagesProducts/image 10.png",
-    discount: 30,
-    price: 113000,
-    oldPrice: 160000,
-  },
-  {
-    id: 4,
-    name: "NIKE AIR-FORCE",
-    image: "/public/assets/ImagesProducts/image 11.png",
-    discount: 30,
-    price: 113000,
-    oldPrice: 160000,
-  },
-  {
-    id: 5,
-    name: "CHOMBA ESSENTIALS ADIDAS",
-    image: "/public/assets/ImagesProducts/image 12.png",
-    discount: 30,
-    price: 113000,
-    oldPrice: 160000,
-  },
-];
+import { useProductoStore } from "../../../store/productoStore";
+import { useEffect } from "react";
 
 const OffersSection = () => {
+  const { productosActivos, fetchProductosActivos } = useProductoStore();
+
+  useEffect(() => {
+    fetchProductosActivos();
+  }, [fetchProductosActivos]);
+
+  const productosConDescuento = productosActivos.filter(
+    (producto) =>
+      producto.descuentos &&
+      producto.descuentos.some((d) => d.activo && d.descuento?.activo)
+  );
+
   return (
     // CONTENEDOR PRINCIPAL DE LA SECCIÓN DE OFERTAS
     <div style={{ background: "black", padding: "24px 0" }}>
@@ -71,16 +42,9 @@ const OffersSection = () => {
           slidesPerView={4}
           style={{ padding: "20px 18px" }}
         >
-          {offers.map((offer) => (
-            <SwiperSlide key={offer.id}>
-              <ProductCard
-                id={offer.id}
-                name={offer.name}
-                image={offer.image}
-                discount={offer.discount}
-                price={offer.price}
-                oldPrice={offer.oldPrice}
-              />
+          {productosConDescuento.map((producto) => (
+            <SwiperSlide key={producto.id}>
+              <ProductCard product={producto} />
             </SwiperSlide>
           ))}
         </Swiper>
