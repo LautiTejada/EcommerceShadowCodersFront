520aa6eb9f0f08c02939426e6730d45a020b2685
Lauti <lautaro3314@hotmail.com>
2025-06-12 21:56:14 -0300

    botones de filtro listos

--- dresscode-front/src/App.tsx
+++ dresscode-front/src/App.tsx
@@ -19,7 +19,6 @@ import { AgregarTiposCategorias } from "./pages/admin/TiposCategorias/AgregarTip
 import { ListaDescuentos } from "./pages/admin/Descuentos/ListaDescuentos/ListaDescuentos";
 import { ListaTiposCategorias } from "./pages/admin/TiposCategorias/ListaTiposCategorias/ListaTiposCategorias";
 
-
 function App() {
   return (
     <Router>
@@ -33,23 +32,32 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/catalog" element={<Catalog />} />
+        <Route
+          path="/catalog/calzados"
+          element={<Catalog filter="CALZADOS" />}
+        />
+        <Route path="/catalog/ropa" element={<Catalog filter="ROPA" />} />
+        <Route path="/catalog/ofertas" element={<Catalog filter="OFERTAS" />} />
 
-      
+        <Route path="/admin" element={<HomeAdmin />} />
 
-        <Route path="/admin" element={<HomeAdmin />}/>
+        <Route path="/admin/add-product" element={<AgregarProducto />} />
+        <Route path="/admin/edit-product" element={<EditarProducto />} />
+        <Route path="/admin/stock-product" element={<StockProducto />} />
+        <Route path="/admin/state-product" element={<EstadoProducto />} />
 
-        <Route path="/admin/add-product" element= {<AgregarProducto/>}/>
-        <Route path="/admin/edit-product" element={<EditarProducto/>}/>
-        <Route path="/admin/stock-product" element={<StockProducto/>}/>
-        <Route path="/admin/state-product" element={<EstadoProducto/>}/>
+        <Route path="/admin/add-discount" element={<AgregarDescuento />} />
+        <Route path="/admin/list-discounts" element={<ListaDescuentos />} />
 
-        <Route path="/admin/add-discount" element={<AgregarDescuento/>}/>
-        <Route path="/admin/list-discounts" element={<ListaDescuentos/>}/>
-
-
-        <Route path="/admin/add-type-cateogory" element={<AgregarTiposCategorias/>}/>
-        <Route path="/admin/list-type-cateogory" element={<ListaTiposCategorias/>}/>
-        
+        <Route
+          path="/admin/add-type-cateogory"
+          element={<AgregarTiposCategorias />}
+        />
+        <Route
+          path="/admin/list-type-cateogory"
+          element={<ListaTiposCategorias />}
+        />
       </Routes>
 
       <Footer />
